ec6b3ff1e0faaa9d66d5856cf1fda6dc3de5a633
Letam Bossman Barinua <letambarinua@gmail.com>
2022-05-12 14:28:30 +0100

    Create a function to generate any number of divs passed to it
    This function will be able to create as many number of divs that is
    passed to it.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,3 +8,34 @@ function generateRandomNumber() {
 function generateRandomColor() {
     return ("rgb("+generateRandomNumber()+","+generateRandomNumber()+","+generateRandomNumber()+")");
 }
+
+//Function to create divs for sketching
+function createInnerDivs(pixel) {
+    //Initializing variables
+    const WidthAndHieght = 350;//Width and height of etch container
+    const pixelSquare = (WidthAndHieght/pixel)+'px';
+    const totalPixels = pixel*pixel;
+
+    //'For' loop to create x number of divs
+    for (let i = 1; i <= totalPixels; i++) {
+
+        //Caching DOM to JS variables
+        let innerD = document.createElement('div');
+        innerD.classList.add('innerDivs');
+        const div = document.querySelector('.etchContainer');
+        div.appendChild(innerD);
+
+        //CSS styling of divs
+        innerD.style.backgroundColor = '#CBCFC2';
+        innerD.style.height = pixelSquare;
+        innerD.style.width = pixelSquare;
+        innerD.style.margin = '0px';
+        innerD.style.border = '0.1px dotted rgba(179,179,179,0.8)';
+        innerD.style.boxSizing = 'border-box';
+        innerD.addEventListener('click', () => {
+        innerD.style.backgroundColor = generateRandomColor();
+        })
+    }
+}
+
+createInnerDivs(4);
