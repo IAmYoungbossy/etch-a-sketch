5816a3dd16bbedad64d3f250848d4c265ced3bb0
Letam Bossman Barinua <letambarinua@gmail.com>
2022-05-15 11:01:35 +0100

    Cache DOM elements to JS variable

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,24 @@
+//DOM CACHING//
+/*modal*/
+const modal = document.querySelector('.modal');
+const maodalContent = document.querySelector('.modal-content');
+let h4 = document.querySelector('.message');
+let input = document.querySelector('.modal-input');
+const okButton = document.querySelector('.ok-btn');
+const cancelButton = document.querySelector('.cancel-btn');
+/*control button*/  
+let colorPicker = document.querySelector('#color');
+let gridText = document.querySelector('#demo');
 let left1 = document.querySelector('#left1');
 let left2 = document.querySelector('#left2');
 let left3 = document.querySelector('#left3');
 let mainControl = document.querySelector('#center-main');
+let range = document.querySelector('#range');
+const right3 = document.querySelector('#right3');
+const right2 = document.querySelector('#right2');
+const right1 = document.querySelector('#right1');
+/*initialize*/
+h4.textContent = 'Please enter a number between 1 and 100.';
 //Function to generate random number between 1 to 256
 function generateRandomNumber() {
     let randomNum = Math.random();
